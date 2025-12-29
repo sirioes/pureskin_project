45e6c24f2fa2730f2b0bb74e030f6a32f12981bb
Risma Putri <wirdianthi05@gmail.com>
2025-12-29 16:55:00 +0800

    feat: our-products, database migrations

---
+++ database/migrations/2025_12_29_065432_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');          // Nama produk
+            $table->integer('price');        // Harga
+            $table->string('image')->nullable(); // Foto produk
+            $table->string('category');     // Kategori produk
+            $table->timestamps();            // created_at dan updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
